9c9df53b80a8d6f5683923b71307729683c447c5
leesharon <leesharooon@gmail.com>
2022-12-14 09:32:32 +0200

    feature codeblock list

--- src/views/Lobby.tsx
+++ src/views/Lobby.tsx
@@ -1,8 +1,9 @@
 import { Codeblock } from 'models/codeblock.interface'
 import { Fragment, FunctionComponent, useEffect, useState } from "react"
 import { useNavigate } from 'react-router-dom'
+import { codeblockService } from 'services/codeblock.service'
 import styled from 'styled-components'
-import { FlexColumn, Heading1 } from '../components/Generics'
+import { FlexColumn, FlexRow, Heading1 } from '../components/Generics'
 import { User } from '../models/user.interface'
 
 interface Props {
@@ -11,10 +12,26 @@ interface Props {
 
 export const Lobby: FunctionComponent<Props> = ({ loggedinUser }) => {
     const navigate = useNavigate()
+    const [codeblock, setCodeblocks] = useState<Codeblock[] | undefined>()
 
     useEffect(() => {
         if (!loggedinUser || !loggedinUser.isMentor) navigate('/')
     }, [loggedinUser, navigate])
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const codeBlocksDB = await codeblockService.query()
+                setCodeblocks(codeBlocksDB)
+            } catch (err) {
+                console.log('Cannot get code blocks', err)
+            }
+        })()
+    }, [setCodeblocks])
+
+    const onSelectCodeblock = (codeblockId: string) => {
+        console.log(codeblockId)
+    }
 
     return (
         <Fragment>
@@ -23,6 +40,13 @@ export const Lobby: FunctionComponent<Props> = ({ loggedinUser }) => {
                     {loggedinUser && `Hello ${loggedinUser.username}, `}
                     Choose a Code Block.
                 </Heading1>
+                {codeblock && <CodeblockList>
+                    {codeblock.map((codeblock, idx) => (
+                        <ListItem key={codeblock._id} onClick={() => { onSelectCodeblock(codeblock._id) }} align='center'>
+                            {idx + 1 + '. ' + codeblock.title}
+                        </ListItem>
+                    ))}
+                </CodeblockList>}
             </LobbyContainer>
         </Fragment>
     )
@@ -30,4 +54,11 @@ export const Lobby: FunctionComponent<Props> = ({ loggedinUser }) => {
 
 const LobbyContainer = styled(FlexColumn)`
     padding-top: 50px;
+`
+
+const CodeblockList = styled(FlexColumn)`
+`
+
+const ListItem = styled(FlexRow)`
+    padding: 15px 10px;
 `
